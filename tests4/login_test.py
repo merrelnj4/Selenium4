4d88c698f3d74c50b678a8d11c756320461ec738
merrelnj1 <merrelnj3@gmail.com>
2021-12-05 17:32:04 -0500

    Adding allure-reports & demo folder

--- tests4/login_test.py
+++ tests4/login_test.py
@@ -26,7 +26,7 @@ class TestLogin():
             homepage.click_welcome()
             homepage.click_logout()
             x = driver.title
-            assert x == "abc"
+            assert x == "OrangeHRM"
 
         except AssertionError as error:
             print("Assertion error occurred")
